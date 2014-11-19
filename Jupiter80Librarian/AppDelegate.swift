2f167d3e1f20cbbc2e98ea89620c5fd2abe5c374
Kim Sand <kimsand@gmail.com>
2014-11-19 19:04:14 +0100

    Initial Commit

---
+++ Jupiter80Librarian/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jupiter80Librarian
+//
+//  Created by Kim André Sand on 19/11/14.
+//  Copyright (c) 2014 Kim André Sand. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ Jupiter80Librarian/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jupiter80Librarian
+//
+//  Created by Kim André Sand on 19/11/14.
+//  Copyright (c) 2014 Kim André Sand. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
