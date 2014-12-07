b3a5219d31a6745d924b782ee865cad79b7427b8
Kim Sand <kimsand@gmail.com>
2014-12-07 07:47:21 +0100

    Reorganized files. Added class for handling SVD structure.

--- Jupiter80Librarian/AppDelegate.swift
+++ Jupiter80Librarian/AppDelegate.swift
@@ -44,7 +44,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 				if fileData != nil && error == nil {
 					NSLog("file length: %d", fileData!.length)
 
-					self.checkValidityOfData(fileData!)
+					SVDUtils.checkValidityOfData(fileData!)
 				} else if error != nil {
 					NSLog("error: %@", error!)
 				}
@@ -54,45 +54,5 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 		}
 	}
 
-	func compareData(data: NSData, loc: Int, bytes: [UInt8]) -> Bool {
-		let subData = data.subdataWithRange(NSRange(location: loc, length: bytes.count))
-		let subBytes = NSData(bytes: bytes, length: bytes.count)
-
-		return subData.isEqualToData(subBytes)
-	}
-
-	func checkValidityOfData(fileData: NSData) -> Bool {
-		// Check for string SVD
-		let isSVDFile = self.compareData(fileData, loc: 2, bytes: [0x53, 0x56, 0x44])
-
-		if !isSVDFile {
-			return false
-		}
-
-		// Check for string REG
-		let isRegFile = self.compareData(fileData, loc: 10, bytes: [0x52, 0x45, 0x47])
-
-		if isRegFile {
-			var fileFormat = FileFormat.Unknown
-
-			// Check for string JPTR
-			let isJupiter80File = self.compareData(fileData, loc: 14, bytes: [0x4a, 0x50, 0x54, 0x52])
-
-			if isJupiter80File {
-				fileFormat = .Jupiter80
-			} else {
-				// Check for string JP50
-				let isJupiter50File = self.compareData(fileData, loc: 14, bytes: [0x4a, 0x50, 0x35, 0x30])
-
-				if isJupiter50File {
-					fileFormat = .Jupiter50
-				}
-			}
-		} else {
-			return false
-		}
-
-		return true;
-	}
 }
 
