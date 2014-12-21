896741ca8c6e9e1689ef1e53c7261bddd30c3c02
Kim Sand <kimsand@gmail.com>
2014-12-21 14:37:55 +0100

    Selecting one or more live sets lists the registrations using these live sets.

--- Jupiter80Librarian/Controllers/LiveSetsListViewController.swift
+++ Jupiter80Librarian/Controllers/LiveSetsListViewController.swift
@@ -9,7 +9,7 @@
 import Cocoa
 
 class LiveSetsListViewController: NSViewController {
-	@IBOutlet var tableView: NSTableView!
+	@IBOutlet var livesTableView: NSTableView!
 	@IBOutlet var orderColumn: NSTableColumn!
 	@IBOutlet var nameColumn: NSTableColumn!
 	@IBOutlet var layer1Column: NSTableColumn!
@@ -17,32 +17,40 @@ class LiveSetsListViewController: NSViewController {
 	@IBOutlet var layer3Column: NSTableColumn!
 	@IBOutlet var layer4Column: NSTableColumn!
 
+	@IBOutlet var regsTableView: NSTableView!
+	@IBOutlet var regNameColumn: NSTableColumn!
+
 	var model = Model.singleton
 	var svdFile: SVDFile?
 	var isInitSound = false
+
+	var registrations: [SVDRegistration] = []
 
 	override func viewDidLoad() {
 		NSNotificationCenter.defaultCenter().addObserver(self, selector: "svdFileDidUpdate:", name: "svdFileDidUpdate", object: nil)
 		super.viewDidLoad()
 
 		self.svdFile = self.model.openedSVDFile
-		self.tableView.reloadData()
+		self.livesTableView.reloadData()
 	}
 
 	func numberOfRowsInTableView(tableView: NSTableView) -> Int {
 		var nrOfRows = 0
 
-		if self.svdFile != nil {
-			nrOfRows = self.svdFile!.liveSets.count
+		if tableView == self.livesTableView {
+			if self.svdFile != nil {
+				nrOfRows = self.svdFile!.liveSets.count
+			}
+		} else if tableView == self.regsTableView {
+			nrOfRows = self.registrations.count
 		}
 
 		return nrOfRows
 	}
 
 	func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
-		// Retrieve to get the view from the pool or,
-		// if no version is available in the pool, load the Interface Builder version
 		var result = tableView.makeViewWithIdentifier(tableColumn!.identifier, owner:self) as NSTableCellView
+
 		result.textField?.textColor = NSColor.blackColor()
 
 		var columnValue: String = ""
@@ -50,38 +58,45 @@ class LiveSetsListViewController: NSViewController {
 
 		let svdLive = self.svdFile!.liveSets[row]
 
-		if tableColumn == self.nameColumn {
-			columnValue = svdLive.liveName
-			textColor = self.textColorForLiveSetName(columnValue)
-		} else if tableColumn == self.orderColumn {
-			columnValue = "\(row + 1)"
-		} else if tableColumn == self.layer1Column
-			|| tableColumn == self.layer2Column
-			|| tableColumn == self.layer3Column
-			|| tableColumn == self.layer4Column
-		{
-			var layerNr: Int
+		if tableView == self.livesTableView {
+			if tableColumn == self.nameColumn {
+				columnValue = svdLive.liveName
+				textColor = self.textColorForLiveSetName(columnValue)
+			} else if tableColumn == self.orderColumn {
+				columnValue = "\(row + 1)"
+			} else if tableColumn == self.layer1Column
+				|| tableColumn == self.layer2Column
+				|| tableColumn == self.layer3Column
+				|| tableColumn == self.layer4Column
+			{
+				var layerNr: Int
 
-			if tableColumn == self.layer1Column {
-				layerNr = 0
-			} else if tableColumn == self.layer2Column {
-				layerNr = 1
-			} else if tableColumn == self.layer3Column {
-				layerNr = 2
-			} else {
-				layerNr = 3
+				if tableColumn == self.layer1Column {
+					layerNr = 0
+				} else if tableColumn == self.layer2Column {
+					layerNr = 1
+				} else if tableColumn == self.layer3Column {
+					layerNr = 2
+				} else {
+					layerNr = 3
+				}
+
+				let layerToneType: SVDPartType = svdLive.layerToneTypes[layerNr]
+				let layerTone: SVDTone? = svdLive.layerTones[layerNr]
+				let layerName: String? = svdLive.layerNames[layerNr]
+
+				if layerTone != nil {
+					columnValue = layerTone!.toneName
+					textColor = self.textColorForToneName(columnValue)
+				} else if layerName != nil {
+					columnValue = layerName!
+					textColor = self.textColorForPartType(layerToneType)
+				}
 			}
-
-			let layerToneType: SVDPartType = svdLive.layerToneTypes[layerNr]
-			let layerTone: SVDTone? = svdLive.layerTones[layerNr]
-			let layerName: String? = svdLive.layerNames[layerNr]
-
-			if layerTone != nil {
-				columnValue = layerTone!.toneName
-				textColor = self.textColorForToneName(columnValue)
-			} else if layerName != nil {
-				columnValue = layerName!
-				textColor = self.textColorForPartType(layerToneType)
+		} else if tableView == self.regsTableView {
+			if tableColumn == self.regNameColumn {
+				let registration = self.registrations[row]
+				columnValue = registration.regName
 			}
 		}
 
@@ -89,6 +104,25 @@ class LiveSetsListViewController: NSViewController {
 		result.textField?.textColor = textColor
 
 		return result
+	}
+
+	func tableViewSelectionDidChange(aNotification: NSNotification) {
+		let tableView = aNotification.object as NSTableView
+
+		if tableView == self.livesTableView {
+			self.registrations.removeAll(keepCapacity: true)
+
+			tableView.selectedRowIndexes.enumerateIndexesUsingBlock {
+				(index: Int, finished: UnsafeMutablePointer<ObjCBool>) -> Void in
+				let svdLive = self.svdFile!.liveSets[index]
+
+				for registration in svdLive.registrations {
+					self.registrations.append(registration)
+				}
+			}
+
+			self.regsTableView.reloadData()
+		}
 	}
 
 	func textColorForPartType(partType: SVDPartType) -> NSColor {
@@ -132,7 +166,7 @@ class LiveSetsListViewController: NSViewController {
 		dispatch_async(dispatch_get_main_queue()) { () -> Void in
 			self.svdFile = self.model.openedSVDFile
 
-			self.tableView.reloadData()
+			self.livesTableView.reloadData()
 		}
 	}
 }
